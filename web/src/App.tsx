ebb78502d905cb6858f2b380ec86dc9bce5aa468
Ivan Tetkic <ivan.tetkic@gmail.com>
2024-04-04 15:26:02 +0200

    MM-10 Initial setup of react application

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './MMC-Logo-40.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img width="130" height="40" src={logo} alt="Logo" />
+    </div>
+  );
+}
+
+export default App;
