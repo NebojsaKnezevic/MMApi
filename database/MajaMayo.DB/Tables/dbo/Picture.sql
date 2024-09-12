54905a12da830969b27f4191a2f835cfe497db46
Ivan Tetkic <ivan.tetkic@ch.abb.com>
2024-09-12 14:59:05 +0200

    MM-35 Initial commit - Backend application

--- database/MajaMayo.DB/Tables/dbo/Picture.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Picture]
-(
-	[Id] INT IDENTITY (1, 1) NOT NULL,
-	[Data] varbinary(max) not null,
-	
-    CONSTRAINT [PK_P] PRIMARY KEY CLUSTERED ([Id] ASC) WITH (FILLFACTOR = 90),
-)
