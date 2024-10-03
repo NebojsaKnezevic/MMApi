ba7f6586c64071971a639f7e8914a3cbca77c8b9
Ivan Tetkic <ivan.tetkic@ch.abb.com>
2024-10-03 15:21:13 +0200

    MM-54 Dodat log i global exception hendler

---
+++ MajaMayo.API/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace MajaMayo.API.Middlewares;
+
+public sealed class GlobalExceptionHandlerMiddleware : IMiddleware
+{
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+
+    private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GlobalExceptionHandlerMiddleware"/> class.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    public GlobalExceptionHandlerMiddleware(ILogger<GlobalExceptionHandlerMiddleware> logger) => _logger = logger;
+
+    /// <inheritdoc />
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Exception occurred: {Message}", ex.Message);
+
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    /// <summary>
+    /// Handles the specified <see cref="Exception"/> for the specified <see cref="HttpContext"/>.
+    /// </summary>
+    /// <param name="context">The HTTP httpContext.</param>
+    /// <param name="exception">The exception.</param>
+    /// <returns>The HTTP response that is modified based on the exception.</returns>
+    private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+
+        if (!context.Response.HasStarted)
+        {
+            context.Response.ContentType = "application/json";
+
+            if (exception.GetType() == typeof(UnauthorizedAccessException))
+            {
+                await context.Response.WriteAsync("Unauthorized access");
+            }
+            else
+            {
+
+
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                await context.Response.WriteAsync("An error occured while processing request.");
+            }
+        }
+
+    }
+
+
+}
