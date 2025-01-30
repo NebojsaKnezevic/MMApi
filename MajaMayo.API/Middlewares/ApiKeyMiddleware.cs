8fbac5fd2b493a8fdcf09f0597fe6ea3e305f040
Nebojsa Knezevic <global021man@gmail.com>
2025-01-30 16:14:33 +0100

    returns all headers if api_key is missing

--- MajaMayo.API/Middlewares/ApiKeyMiddleware.cs
+++ MajaMayo.API/Middlewares/ApiKeyMiddleware.cs
@@ -20,10 +20,20 @@ namespace MajaMayo.API.Middlewares
 
             if (context.Request.Path.HasValue && context.Request.Path.Value.Contains("DeltaGenerali/RegisterDGUsers"))
             {
+                //if (!context.Request.Headers.TryGetValue("API_KEY", out var extractedApiKey))
+                //{
+                //    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                //    await context.Response.WriteAsync("API Key is missing.");
+                //    return;
+                //}
                 if (!context.Request.Headers.TryGetValue("API_KEY", out var extractedApiKey))
                 {
                     context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                    await context.Response.WriteAsync("API Key is missing.");
+                    context.Response.ContentType = "text/plain";
+
+                    var headersAsString = string.Join(Environment.NewLine, context.Request.Headers.Select(header => $"{header.Key}: {header.Value}"));
+
+                    await context.Response.WriteAsync($"API Key is missing.\n\n{headersAsString}");
                     return;
                 }
 
